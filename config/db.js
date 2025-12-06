6e88493cc8fe1e7a34d131ae56661b79298fc6b1
SQUIDNOVA <you@example.comsquidnova04@gmail.com>
2025-12-06 19:03:11 +0530

    link fix

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const createDB = async () => {
     try {
-        await mongoose.connect(process.env.mongoose_link);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log(" MongoDB connected successfully");
     } catch (error) {
         console.error(` Error connecting to MongoDB: ${error}`);
